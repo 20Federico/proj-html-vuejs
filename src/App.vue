3ff32bcdcf260e63ec616db46e4021ce9b21599a
20Federico <fefe.molino@gmail.com>
2021-11-22 20:52:17 +0100

    added fontAwesome

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,7 @@
 </template>
 
 <script>
+import "@fortawesome/fontawesome-free/js/all.js";
 import 'bootstrap';
 import Footer from '@/components/Footer.vue'
 import Header from '@/components/Header.vue'
