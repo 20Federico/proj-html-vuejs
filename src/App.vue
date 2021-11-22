d05e2127c6f4076e7ec3d87165a65112d3e045af
20Federico <fefe.molino@gmail.com>
2021-11-22 17:05:34 +0100

    scaffolding

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,31 @@
 <template>
   <div id="app">
-    
+    <Header />
+    <Main />
+    <Footer />
   </div>
 </template>
 
 <script>
+import Footer from './components/Footer.vue'
+import Header from './components/Header.vue'
+import Main from './components/Main.vue'
 //import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    
+    Header,
+    Main,
+    Footer
   }
 }
 </script>
 
 <style lang="scss">
+@import "@/styles/variables.scss";
+
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
