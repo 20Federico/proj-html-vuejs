9c0ba1626a0f314b633f56c19009e6f94e7e5e34
20Federico <fefe.molino@gmail.com>
2021-11-23 23:37:39 +0100

    removed style tag

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,28 +1,37 @@
 <template>
   <main>
+    <MainOptionTagFixed />
     <MainStatsTable />
     <MainContentTable />
+    <MainCoursesCards />
+    <MainTextAndCards />
     <MainSponsorTable />
+    <MainReviewSection />
+    <MainDailyArticlesCards />
   </main>
 </template>
 
 <script>
-import MainContentTable from './MainContentTable.vue';
-import MainSponsorTable from './MainSponsorTable.vue';
 import MainStatsTable from './MainStatsTable.vue';
+import MainContentTable from './MainContentTable.vue';
+import MainCoursesCards from './MainCoursesCards.vue';
+import MainTextAndCards from './MainTextAndCards.vue';
+import MainSponsorTable from './MainSponsorTable.vue';
+import MainReviewSection from './MainReviewSection.vue';
+import MainDailyArticlesCards from './MainDailyArticlesCards.vue';
+import MainOptionTagFixed from './MainOptionTagFixed.vue';
 
 export default {
+  name: "Main",
   components: { 
     MainStatsTable,
     MainContentTable,
-    
-    MainSponsorTable 
-  },
-  name: "Main",
+    MainCoursesCards,
+    MainTextAndCards,
+    MainSponsorTable,
+    MainReviewSection,
+    MainDailyArticlesCards,
+    MainOptionTagFixed 
+  }
 }
 </script>
-
-<style lang="scss" scoped>
-@import "@/styles/variables.scss";
-
-</style>
