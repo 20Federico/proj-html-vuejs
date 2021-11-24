7da3da90df111d2e93f994e9bbe1db001038aed0
20Federico <fefe.molino@gmail.com>
2021-11-24 16:31:13 +0100

    completed dinamic review section

--- src/components/MainReviewSection.vue
+++ src/components/MainReviewSection.vue
@@ -4,12 +4,10 @@
     <div class="box d-flex justify-content-center align-items-center">
       <div class="review_container text-center">
         <p class="text_primary fw-bold mb-5">REAL STORIES</p>
-        <p class="heading_grey fw-bold mb-5">
-          I am free to learn at my own pace, follow my own schedule and choose the subject I want to learn from syllabus. Great study portal for people like me.
-        </p>
-        <img src="@/assets/testimonial-avata-01.jpg" alt="testimonial avatar">
-        <p class="heading_grey fw-bold">Mina Hollace</p>
-        <p class="text_small">/ Freelancer</p>
+        <p class="heading_grey fw-bold mb-5">{{reviews[activeReview].text}}</p>
+        <img :src="require('@/assets/' + reviews[activeReview].profileImgSrc)" alt="testimonial avatar">
+        <p class="heading_grey fw-bold">{{reviews[activeReview].name}}</p>
+        <p class="text_small">/ {{reviews[activeReview].career}}</p>
       </div>
     </div>
     <div class="box">
@@ -17,9 +15,13 @@
     </div>
   </div>
   <div class="change_review d-flex flex-column justify-content-center align-items-center">
-      <i class="fas fa-caret-up"></i>
-      <p class="m-0">1/4</p>
-      <i class="fas fa-caret-down"></i>
+      <div @click="prevReview()" class="icon_box">
+        <i class="fas fa-caret-up"></i>
+      </div>
+      <p class="m-0">{{activeReview + 1}}/{{reviews.length}}</p>
+      <div class="icon_box" @click="nextReview()">
+        <i class="fas fa-caret-down"></i>
+      </div>
     </div>
 </div>
 </template>
@@ -27,6 +29,55 @@
 <script>
 export default {
   name: 'MainReviewSection',
+  data() {
+    return {
+      activeReview: 2,
+      reviews: [
+        {
+          text: 'I am free to learn at my own pace, follow my own schedule and choose the subject I want to learn from syllabus. Great study portal for people like me.',
+          profileImgSrc: 'testimonial-avata-02.jpg',
+          name: 'Mina Hollace',
+          career: 'Freelancer'
+        },
+        {
+          text: 'free to learn at my own pace, follow my own schedule syllabus. Great study portal for people like me.',
+          profileImgSrc: 'testimonial-avata-01.jpg',
+          name: 'Tom Clancy',
+          career: 'Employee'
+        },
+        {
+          text: 'Learn am free to at my own pace, follow my own schedule and choose the subject I want to learn from syllabus. Great study portal.',
+          profileImgSrc: 'testimonial-avata-03.jpg',
+          name: 'George Crumble',
+          career: 'Freelancer'
+        },
+        {
+          text: 'I am free to learn at my own pace, follow my own schedule and choose the subject I want to learn from syllabus. Great study portal for people like me. I am free to learn at my own pace, follow my own schedule and choose the subject I want to learn from syllabus.',
+          profileImgSrc: 'testimonial-avata-04.jpg',
+          name: 'Mark Lexus',
+          career: 'Employee'
+        }
+      ]
+    }
+  },
+  methods: {
+    prevReview() {
+      if (this.activeReview < (this.reviews.length - 1)) {
+        this.activeReview++
+      } else {
+        this.activeReview = 0
+      }
+      return this.activeReview
+    },
+    nextReview() {
+      if (this.activeReview > 0) {
+        this.activeReview--
+      } else {
+        this.activeReview = (this.reviews.length - 1)
+      }
+      return this.activeReview
+    }
+  }
 }
 </script>
 
@@ -35,6 +86,7 @@ export default {
 
 .my_container {
   position: relative;
+  overflow: hidden;
 }
 .img_cover {
   width: 100%;
